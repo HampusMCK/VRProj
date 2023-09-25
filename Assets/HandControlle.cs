a2701c779194b1c1b44512d6ab90b28a5e2b2534
Hampus Eriksson <Hampus.Eriksson@elev.ga.ntig.se>
2023-09-25 11:36:27 +0200

    Initial Commit

---
+++ Assets/HandControlle.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class HandControlle : MonoBehaviour
+{
+    [SerializeField]
+    GameObject BulletPrefab;
+
+    ActionBasedController controller;
+
+    void Awake()
+    {
+        controller = GetComponent<ActionBasedController>();
+
+        controller.activateAction.action.started += OnTriggerPress;
+    }
+
+    void OnTriggerPress(InputAction.CallbackContext context)
+    {
+        Instantiate(BulletPrefab, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
